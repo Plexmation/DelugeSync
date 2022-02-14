b3cb6afd1ce09238deb675e9a76a75ffb0971484
sjdaniel10 <sjdaniel6241@gmail.com>
2022-02-14 23:55:49 +0200

    testing dirs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 addgroup --system --gid 1000 customgroup \
 && adduser --system --uid 1000 --ingroup customgroup --shell /bin/sh customuser
+mkdir -p /app/files
+mkdir -p /app/files/sonarr
+mkdir -p /app/files/radarr
+chmod 777 -R /app/files
 exec runuser -u customuser "$@"
